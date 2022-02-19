51fc2617fd26f39a7177ba90355d1546a560d685
cupcake08 <bhankhariaa@gmail.com>
2022-02-19 11:05:49 +0530

    added: more sizes

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -19,14 +19,21 @@ class Dimensions {
   static double width30 = screenHeight / 28.13;
   static double width45 = screenHeight / 18.76;
   //dynamic font size
+  static double font16 = screenHeight / 52.75;
   static double font20 = screenHeight / 42.2;
+  static double font26 = screenHeight / 32.46;
   //dynamic border radius
   static double radius20 = screenHeight / 42.2;
   static double radius30 = screenHeight / 28.13;
   static double radius15 = screenHeight / 56.27;
   //icon Size
   static double iconSize24 = screenHeight / 35.17;
+  static double iconSize16 = screenHeight / 52.75;
   //list view size
   static double listViewImgSize = screenWidth / 3.25;
   static double listViewTextContSize = screenWidth / 3.9;
+  //popular food detail
+  static double popularFoodImgSize = screenHeight / 2.41;
+  //bottom bar
+  static double bottomHeightBar = screenHeight / 7.03;
 }
