acb8d4a7155c25d7e81c4cdd55703bbfa37b91c5
cupcake08 <bhankhariaa@gmail.com>
2022-02-21 11:14:07 +0530

    api information constants

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
+class AppConstants {
+  static const String APP_NAME = "ABFood";
+  static const int API_VERSION = 1;
+  static const String BASE_URL = "https://mvs.bslmeiyu.com";
+  static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
+  static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/popular";
+  static const String TOKEN = "abtOKEN";
+}
