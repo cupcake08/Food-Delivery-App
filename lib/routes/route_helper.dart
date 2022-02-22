803099f0a82ab811593bd09593d4818081218fb9
cupcake08 <bhankhariaa@gmail.com>
2022-02-22 21:46:26 +0530

    routes class

---
+++ lib/routes/route_helper.dart
@@ -0,0 +1,38 @@
+import 'package:food_delivery/pages/food/popular_food_detail.dart';
+import 'package:food_delivery/pages/food/recommended_food_detail.dart';
+import 'package:food_delivery/pages/home/main_food_page.dart';
+import 'package:get/get.dart';
+
+class RouteHelper {
+  static const String initial = "/";
+  static const String popularRoute = "/popular-food-details";
+  static const String recommendedRoute = "/recommended-food-details";
+
+  static String getPopularPage(int pageId) => "$popularRoute?pageId=$pageId";
+  static String getInitial() => initial;
+  static String getRecommendedPage(int pageId) =>
+      "$recommendedRoute?pageId=$pageId";
+
+  static List<GetPage> routes = [
+    GetPage(
+      name: initial,
+      page: () => const MainFoodPage(),
+    ),
+    GetPage(
+      name: popularRoute,
+      page: () {
+        String? pageId = Get.parameters['pageId'];
+        return PopularFoodDetail(pageId: int.parse(pageId!));
+      },
+      transition: Transition.fadeIn,
+    ),
+    GetPage(
+      name: recommendedRoute,
+      page: () {
+        String? pageId = Get.parameters['pageId'];
+        return RecommendedFoodDetail(pageId: int.parse(pageId!));
+      },
+      transition: Transition.fadeIn,
+    )
+  ];
+}
