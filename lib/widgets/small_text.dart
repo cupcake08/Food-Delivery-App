984adeab8f0fb879d9aa39480188249e1058b61b
cupcake08 <bhankhariaa@gmail.com>
2022-02-22 21:47:02 +0530

    fix: maxLines and text_overflow

--- lib/widgets/small_text.dart
+++ lib/widgets/small_text.dart
@@ -7,15 +7,21 @@ class SmallText extends StatelessWidget {
     required this.text,
     this.size = 12,
     this.height = 1.2,
+    this.overflow,
+    this.maxLines,
   }) : super(key: key);
   final Color color;
   final String text;
   final double size;
   final double height;
+  final TextOverflow? overflow;
+  final int? maxLines;
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      maxLines: maxLines,
+      overflow: overflow,
       style: TextStyle(
         color: color,
         fontWeight: FontWeight.w400,
